41371565bb6591001e780cf3f332ec5e7b6a4f77
thameemk612 <root@localhost.localdomain>
2018-11-02 00:43:30 +0530

    contact.js added

--- site_data/js/contact.js
+++ site_data/js/contact.js
@@ -1,33 +1,33 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzoQZdJY0slRnYbIH2gJa1YdmmgvFD7Fpo2p7qyNEGeFe6Czq8/exec'
-const form = document.forms['email-from-public'];
-const alert_sub = document.getElementById('alert_submission');
+const scriptURLC = 'https://script.google.com/macros/s/AKfycbzoQZdJY0slRnYbIH2gJa1YdmmgvFD7Fpo2p7qyNEGeFe6Czq8/exec'
+const formc = document.forms['email-from-public'];
+const alert_subc = document.getElementById('alert_submission');
 
-form.addEventListener('submit', e => {
+formc.addEventListener('submit', e => {
 	// todo disable submit button
 	e.preventDefault();
-	fetch(scriptURL, {
+	fetch(scriptURLC, {
 			method: 'POST',
-			body: new FormData(form)
+			body: new FormData(formc)
 		})
 		.then(res => {
 			console.log(res);
 			// todo enable submit button
 			if (res['status'] == 200) {
-				alert_sub.classList.remove('alert-danger');
-				alert_sub.classList.add('alert-msg');
-				alert_sub.innerHTML = 'Message Recieved!!. We will contact you soon';
-				form.reset();
+				alert_subc.classList.remove('alert-danger');
+				alert_subc.classList.add('alert-msg');
+				alert_subc.innerHTML = 'Message Recieved!!. We will contact you soon';
+				formc.reset();
 			} else {
-				alert_sub.classList.remove('alert-msg');
-				alert_sub.classList.add('alert-danger');
-				alert_sub.innerHTML = 'Error occured.';
+				alert_subc.classList.remove('alert-msg');
+				alert_subc.classList.add('alert-danger');
+				alert_subc.innerHTML = 'Error occured.';
 			}
 		})
 		.catch(error => {
 			console.error('Error!', error.message);
-			alert_sub.classList.remove('alert-msg');
-			alert_sub.classList.add('alert-danger');
-			alert_sub.innerHTML = 'Error occured.';
+			alert_subc.classList.remove('alert-msg');
+			alert_subc.classList.add('alert-danger');
+			alert_subc.innerHTML = 'Error occured.';
 			// todo enable submit button
 		})
 });
