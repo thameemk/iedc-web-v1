0a2876f700a909b4a472c9e830bc72f6c6ea2482
thameemk612 <thameemk612@gmail.com>
2019-08-09 00:26:34 +0530

    web adin

--- application/views/static/contact.php
+++ application/views/static/contact.php
@@ -8,9 +8,9 @@
 						</h1>
 						<h4 class="head-font" style="color:#ff2d00;"><?= $this->session->flashdata('success') ?></h4><?= $this->session->flashdata('true') ?>
                                 <h4 class="head-font" style="color:#ff2d00;"><?= $this->session->flashdata('fail') ?></h4>
-                             
+
 						<form class="appoinment-form" action="<?=base_url()?>Contact/postEmail" method="post">
-							
+
 
 													<div class="row">
 															<div class="col-lg-12 d-flex flex-column">
@@ -20,20 +20,23 @@
 																	<input name="email" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" class="form-control mt-20" required type="email">
 															</div>
 															<div class="col-lg-6 d-flex flex-column">
-																	
+
 																	<input name="subject" placeholder="Subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Subject'" class="form-control mt-20" required type="text">
 															</div>
 
 
 															<div class="col-lg-12 flex-column">
 																	<textarea  rows="5" class="form-control mt-20" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" required></textarea>
-															</div>																												
+															</div>
 													</div>
 													<button type="submit" class="mt-3 ui inverted green button  ">Send</button>
-															
+
 											</form>
 									 </div>
 							</div>
 					</div>
 			</section>
 		<!-- End Contact Area -->
+		<section>
+			<h4 class="mt-3 mb-3 text-center">For any web related queries contact <a href="mailto:webadmin@iedctkmce.com">webadmin@iedctkmce.com</a></h4>
+		</section>
