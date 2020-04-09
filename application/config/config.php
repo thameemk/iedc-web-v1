f8c29536c06b64d18e671e2364247687e4ee7d99
thameemk612 <thameemk612@gmail.com>
2020-04-09 23:58:36 +0530

    db

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/IEDC-Dynamic/';
+$config['base_url'] = 'http://localhost/iedc-web-v1/';
 
 /*
 |--------------------------------------------------------------------------
